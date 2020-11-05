8b7d5157b866f7c8d837dc1074526b537975f837
Spencer Ye <sye348@student.fuhsd.org>
2020-11-04 21:34:14 -0800

    test

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -2,6 +2,8 @@ var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 ctx.beginPath();
 ctx.rect(20, 40, 50, 50);
+ctx.rect(80, 40, 50, 50);
 ctx.fillStyle = "#FF0000";
+
 ctx.fill();
 ctx.closePath();
