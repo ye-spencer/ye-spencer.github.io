c74f36170b66cdd2ac6070e8f609c5976df44634
Spencer Ye <sye348@student.fuhsd.org>
2020-11-07 20:38:37 -0800

    update bouncing

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -1,22 +1,33 @@
-var canvas = document.getElementById("gameCanvas");
+var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
-
+var ballRadius = 10;
 var x = canvas.width/2;
 var y = canvas.height-30;
-
 var dx = 2;
 var dy = -2;
 
-function draw() 
+function drawBall() 
 {
-	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	
-    ctx.beginPath();
-	ctx.arc(x, y, 10, 0, Math.PI*2);
+	ctx.beginPath();
+	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
 	ctx.fillStyle = "#0095DD";
 	ctx.fill();
 	ctx.closePath();
-	x += dx
-	y += dy
 }
+
+function draw() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+	drawBall();
+
+	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+	dx = -dx;
+	}
+	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+	dy = -dy;
+	}
+
+	x += dx;
+	y += dy;
+}
+
 setInterval(draw, 10);
