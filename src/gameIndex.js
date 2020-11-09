b0bacbaee8068581c581c9da4c9101b5b1e21d39
Spencer Ye <sye348@student.fuhsd.org>
2020-11-08 20:25:15 -0800

    Update game v2

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -1,16 +1,16 @@
 var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
-var x = canvas.width/2;
-var y = canvas.height-30;
+var x = canvas.width / 2;
+var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
 function drawBall() 
 {
 	ctx.beginPath();
-	ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-	ctx.fillStyle = "#0095DD";
+	ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
+	ctx.fillStyle = "#0095FF";
 	ctx.fill();
 	ctx.closePath();
 }
@@ -19,10 +19,10 @@ function draw() {
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	drawBall();
 
-	if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+	if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
 	dx = -dx;
 	}
-	if(y + dy > canvas.height-ballRadius || y + dy < ballRadius) {
+	if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
 	dy = -dy;
 	}
 
