536c17eb4c1c9153ee9fade0fb21d5b15692f111
Spencer Ye <sye348@student.fuhsd.org>
2020-11-11 20:59:21 -0800

    U

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -6,6 +6,12 @@ var y = canvas.height - 30;
 var dx = 2;
 var dy = -2;
 
+var rightPressed = false;
+var leftPressed = false;
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
 function drawBall() 
 {
 	ctx.beginPath();
@@ -31,6 +37,24 @@ function draw()
 
 	x += dx;
 	y += dy;
+}
+
+function keyDownHandler(e) {
+	if(e.key == "Right" || e.key == "ArrowRight") {
+		rightPressed = true;
+	}
+	else if(e.key == "Left" || e.key == "ArrowLeft") {
+		leftPressed = true;
+    }
+}
+
+function keyUpHandler(e) {
+    if(e.key == "Right" || e.key == "ArrowRight") {
+        rightPressed = false;
+    }
+    else if(e.key == "Left" || e.key == "ArrowLeft") {
+        leftPressed = false;
+    }
 }
 
 setInterval(draw, 10);
