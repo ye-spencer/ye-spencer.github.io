4ce6a8735dc7674d2128a981b497e5c2191fd652
Spencer Ye <sye348@student.fuhsd.org>
2020-11-13 20:35:34 -0800

    update game

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -1,4 +1,4 @@
-var canvas = document.getElementById("myCanvas");
+var canvas = document.getElementById("gameCanvas");
 var ctx = canvas.getContext("2d");
 var ballRadius = 10;
 var x = canvas.width/2;
