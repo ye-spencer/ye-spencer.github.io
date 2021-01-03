412944b2d7b09430899d65118e4e033548eb8198
Spencer Ye <sye348@student.fuhsd.org>
2021-01-02 21:17:21 -0800

    create game ending

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -69,9 +69,15 @@ function draw()
 	{
 		dx = -dx;
 	}
-	if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) 
+	if(x > paddleX && x < paddleX + paddleWidth) 
 	{
 		dy = -dy;
+	}
+	else 
+	{
+		alert("GAME OVER");
+		document.location.reload();
+		clearInterval(interval);
 	}
 
 	if(rightPressed) 
