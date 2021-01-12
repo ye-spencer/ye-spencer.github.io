bf2b3abab5c9082da158d46538d3afa3d7abca06
Spencer Ye <sye348@student.fuhsd.org>
2021-01-11 22:20:39 -0800

    update bricks

--- src/gameIndex.js
+++ src/gameIndex.js
@@ -10,10 +10,46 @@ var paddleWidth = 75;
 var paddleX = (canvas.width - paddleWidth) / 2;
 var rightPressed = false;
 var leftPressed = false;
+
+var brickRowCount = 3;
+var brickColumnCount = 5;
+var brickWidth = 75;
+var brickHeight = 20;
+var brickPadding = 10;
+var brickOffsetTop = 30;
+var brickOffsetLeft = 30;
+
 const paddleSpeed = 7;
 console.log("program started")
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+
+var bricks = [];
+for(var c = 0; c < brickColumnCount; c++) 
+{
+	bricks[c] = [];
+	for(var r = 0; r < brickRowCount; r++) 
+	{
+		bricks[c][r] = { x: 0, y: 0 };
+	}
+}
+
+function drawBricks() 
+{
+	for(var c = 0; c < brickColumnCount; c++) 
+	{
+		for(var r = 0; r < brickRowCount; r++) 
+		{
+			bricks[c][r].x = 0;
+			bricks[c][r].y = 0;
+			ctx.beginPath();
+			ctx.rect(0, 0, brickWidth, brickHeight);
+			ctx.fillStyle = "#0095DD";
+			ctx.fill();
+			ctx.closePath();
+		}
+	}
+}
 
 function keyDownHandler(key) 
 {
