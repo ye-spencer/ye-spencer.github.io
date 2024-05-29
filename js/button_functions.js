00f8c1eb90d1939063295813c732c7ea1c3fadff
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-05-28 22:50:00 -0700

    add some JQuery

--- js/button_functions.js
+++ js/button_functions.js
@@ -1,16 +1,28 @@
-function buttonJumpToElement()
-{
-	var button_section = this.getAttribute('data-name');
-	var parent_document = this;
-	while (!(parent_document instanceof HTMLDocument))
-	{
-		parent_document = parent_document.parentNode;
-	}
-    parent_document.getElementById(button_section).scrollIntoView();
-}
+// function buttonJumpToElement()
+// {
+// 	var button_section = this.getAttribute('data-name');
+// 	var parent_document = this;
+// 	while (!(parent_document instanceof HTMLDocument))
+// 	{
+// 		parent_document = parent_document.parentNode;
+// 	}
+//     parent_document.getElementById(button_section).scrollIntoView();
+// }
 
-var elements = this.document.getElementsByClassName("directionButton");
-for (let i = 0; i < elements.length; i++) 
+// var elements = this.document.getElementsByClassName("directionButton");
+// for (let i = 0; i < elements.length; i++) 
+// {
+// 	elements[i].addEventListener("click", buttonJumpToElement);
+// }
+
+$(document).ready(function()
 {
-	elements[i].addEventListener("click", buttonJumpToElement);
-}
+  $("#aboutMeButton").click(function()
+  {
+    $("#aboutMe")[0].scrollIntoView({behavior: "smooth"});
+  });
+  $("#projectsButton").click(function()
+  {
+    $("#projects")[0].scrollIntoView({behavior: "smooth"});
+  });
+});
