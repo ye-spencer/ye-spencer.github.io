9b3be8a45d89ea3b5afbf6b9999d13d3f3f85cab
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-05-30 20:45:23 -0700

    add border blocker

--- js/button_functions.js
+++ js/button_functions.js
@@ -1,20 +1,3 @@
-// function buttonJumpToElement()
-// {
-// 	var button_section = this.getAttribute('data-name');
-// 	var parent_document = this;
-// 	while (!(parent_document instanceof HTMLDocument))
-// 	{
-// 		parent_document = parent_document.parentNode;
-// 	}
-//     parent_document.getElementById(button_section).scrollIntoView();
-// }
-
-// var elements = this.document.getElementsByClassName("directionButton");
-// for (let i = 0; i < elements.length; i++) 
-// {
-// 	elements[i].addEventListener("click", buttonJumpToElement);
-// }
-
 $(document).ready(function()
 {
   $("#aboutMeButton").click(function()
