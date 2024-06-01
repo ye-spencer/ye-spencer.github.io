626781ca5a1f45ce3028cc16002af854fc6e6f08
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-05-31 21:59:57 -0700

    fix div layering

--- js/button_functions.js
+++ js/button_functions.js
@@ -2,18 +2,18 @@ $(document).ready(function()
 {
   $("#aboutMeButton").click(function()
   {
-    $("#aboutMe")[0].scrollIntoView({behavior: "smooth"});
+    $("#aboutMe")[0].scrollIntoView({behavior: "smooth", inline: 'center', block: 'center'});
   });
   $("#projectsButton").click(function()
   {
-    $("#projects")[0].scrollIntoView({behavior: "smooth"});
+    $("#projects")[0].scrollIntoView({behavior: "smooth", inline: 'center', block: 'center'});
   });
   $("#blogButton").click(function()
   {
-    $("#blog")[0].scrollIntoView({behavior: "smooth"});
+    $("#blog")[0].scrollIntoView({behavior: "smooth", inline: 'center', block: 'center'});
   });
   $("#contactButton").click(function()
   {
-    $("#contact")[0].scrollIntoView({behavior: "smooth"});
+    $("#contact")[0].scrollIntoView({behavior: "smooth", inline: 'center', block: 'center'});
   });
 });
