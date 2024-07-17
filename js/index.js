f4057376cafce8dc259b7eada4ba8fbc4e6ff6a1
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-07-17 16:35:25 +0800

    continue testing

--- js/index.js
+++ js/index.js
@@ -2,8 +2,8 @@ import { LikeButton } from '/react_test.js';
 
 console.log("SDF");
 
-// const e = React.createElement;
+const e = React.createElement;
 
-// const domContainer = document.querySelector('#like_button_container');
-// const root = ReactDOM.createRoot(domContainer);
-// root.render(e(LikeButton));
+const domContainer = document.querySelector('#like_button_container');
+const root = ReactDOM.createRoot(domContainer);
+root.render(e(LikeButton));
