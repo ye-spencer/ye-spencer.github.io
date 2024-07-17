6b913ac48bd627da49d747fda8bff71c19d063ec
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-07-17 16:37:33 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -1,9 +1,11 @@
 import { LikeButton } from '/react_test.js';
 
-console.log("SDF");
+console.log("Successful Import");
 
 const e = React.createElement;
 
 const domContainer = document.querySelector('#like_button_container');
 const root = ReactDOM.createRoot(domContainer);
 root.render(e(LikeButton));
+
+console.log("Successful Display");
