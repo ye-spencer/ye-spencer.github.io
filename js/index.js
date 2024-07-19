3ff60c3c832609bc749841f6c036029b0d39809f
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-07-19 16:18:27 +0800

    create project slider with react and js

--- js/index.js
+++ js/index.js
@@ -2,39 +2,36 @@ console.log("Loading Version 0.0.4");
 
 const createEle = React.createElement;
 
-class LikeButton extends React.Component 
+class projectSlider extends React.Component 
 {
-  constructor(props) {
+  constructor(props) 
+  {
     super(props);
   }
 
-  render() {
-
-    return createEle(
-      'button',
-      { className: 'testClass' },
-      'BooHoo'
-    );
+  render() 
+  {
+    return createProjectSlider("github.png", "google.com", "Testing Component", "Awesomeness", "THis works!");
   }
 }
 
-// function createProjectSlider ({ imgName, link, projectName, skillsUsed, description }) 
-// {
-//   return createEle(
-//     'div',
-//     { className: "projectSlider" },
-//     createEle('img', {source: ("imgs/" + imgName), className = "projectPicture"}),
-//     createEle('a', {href: link}, createEle('br', null), projectName),
-//     createEle('h5', {className: "projectSkillsUsed"}, skillsUsed),
-//     createEle('p', {className: "projectDescription"}, description)
-//   );
-// }
+function createProjectSlider ( imgName, link, projectName, skillsUsed, description ) 
+{
+  return createEle(
+    'div',
+    { className: "projectSlider" },
+    createEle('img', {source: ("imgs/" + imgName), className = "projectPicture"}),
+    createEle('a', {href: link}, createEle('br', null), projectName),
+    createEle('h5', {className: "projectSkillsUsed"}, skillsUsed),
+    createEle('p', {className: "projectDescription"}, description)
+  );
+}
 
 console.log("Successful Import");
 
 
-const domContainer = document.querySelector('#like_button_container');
-const root = ReactDOM.createRoot(domContainer);
-root.render(createEle(LikeButton));
+const domContainer = document.querySelector('#like_button_container'); // Find the Container to put it in
+const root = ReactDOM.createRoot(domContainer); // Create a React root to render in, the container is a DIV
+root.render(createEle(projectSlider)); // render the elements
 
 console.log("Successful Display");
