cdab55d261977bda45537f828e577c38c48a9f6b
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-07-23 15:08:50 +0800

    attempt to create sliders with reusable components

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-console.log("Loading Version 0.1.6");
+console.log("Loading Version 0.2.0");
 
 const createEle = React.createElement;
 
@@ -7,13 +7,36 @@ class projectSlider extends React.Component
   constructor(props) 
   {
     super(props);
-    this.state = props
+    this.state = props;
   }
 
   render() 
   {
     return createProjectSlider(this.state.img, this.state.link, this.state.title, this.state.skills, this.state.desc);
   }
+}
+
+class projectBoard extends React.Component
+{
+  constructor(props)
+  {
+    super(props);
+  }
+
+  render()
+  {
+    return createProjectBoard();
+  }
+}
+
+function createProjectBoard()
+{
+  return createEle(
+    'div',
+    null,
+    createEle(projectSlider, {img: "github.png", link: "../index.html", title: "This Website", skills: "HTML, CSS, JavaScript, React, jQuery", desc: "I designed and built this website completely from scratch. I had little understanding of frontend before this website, and a lot of the learning was through slowly learning"}),
+    createEle(projectSlider, {img: "github.png", link: "../index.html", title: "Test", skills: "HTML, CSS, JavaScript, React, jQuery", desc: "I designed and built this website completely from scratch. I had little understanding of frontend before this website, and a lot of the learning was through slowly learning"}),
+    createEle(projectSlider, {img: "github.png", link: "../index.html", title: "Test", skills: "HTML, CSS, JavaScript, React, jQuery", desc: "I designed and built this website completely from scratch. I had little understanding of frontend before this website, and a lot of the learning was through slowly learning"}))
 }
 
 function createProjectSlider ( imgName, link, projectName, skillsUsed, description ) 
@@ -33,8 +56,6 @@ console.log("Successful Import");
 
 const projectsDOM = document.querySelector('#projectA'); // Find the Container to put it in
 const projectsRoot = ReactDOM.createRoot(projectsDOM); // Create a React root to render in, the container is a DIV
-projectsRoot.render(
-  createEle(projectSlider, {img: "github.png", link: "../index.html", title: "This Website", skills: "HTML, CSS, JavaScript, React, jQuery", desc: "I designed and built this website completely from scratch. I had little understanding of frontend before this website, and a lot of the learning was through slowly learning"})
-  ); // render the elements
+projectsRoot.render(createEle(projectBoard, null)); // render the elements
 
 console.log("Successful Display");
