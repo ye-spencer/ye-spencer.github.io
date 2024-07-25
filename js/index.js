b283cbd69827d70be5f228d3600c5480cb59c43f
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-07-25 14:42:45 +0800

    update blog tag format

--- js/index.js
+++ js/index.js
@@ -95,7 +95,7 @@ function createBlogBox (type, num)
   {
     classNames += "column-1";
   }
-  return createEle("div", {className: classNames, onClick: () => {window.open(("blogs/" + blogTitles[num] + ".html"), '_blank');}}, createEle("h6", {className: "blogTitle"}, blogTitles[num]), createEle("p", {className: "blogTag"}, blogTags[num]));
+  return createEle("div", {className: classNames, onClick: () => {window.open(("blogs/" + blogTitles[num] + ".html"), '_blank');}}, createEle("h6", {className: "blogTitle"}, blogTitles[num]), createEle("p", {className: "blogTag"}, "Tags: " + blogTags[num]));
 }
 
 function createRow(size, startingPoint)
