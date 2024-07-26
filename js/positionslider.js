608c4e9433c4b9d9c5c4c8e96a4ff120fbef85c6
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-07-26 17:51:11 +0800

    fix bug in calculating indexes

--- js/positionslider.js
+++ js/positionslider.js
@@ -6,7 +6,7 @@ const slider = $("#positionTyper")[0];
 function changeTitle () {
 	slider.style.animation = "slideOut 1s forwards";
 	setTimeout( () => {
-		currentIndex = (currentIndex + 1) positions.length;
+		currentIndex = (currentIndex + 1) % positions.length;
 		slider.textContent = positions[currentIndex];
 		slider.style.animation = 'slideIn 1s forwards';
 	}, 1000);
